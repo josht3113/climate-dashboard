97ebc700ae0048cafd4e055459326c8c704a92e9
josht3113 <josht3113@yahoo.com>
2026-04-09 13:26:55 -0400

    Delete src/pages directory

--- src/pages/NortheastClimate.jsx
+++
@@ -1,9 +0,0 @@
-export default function NortheastClimate() {
-  return (
-    <iframe
-      src="https://josht3113.github.io/northeast-climate-dashboard/"
-      style={{ width:'100%', height:'100vh', border:'none', display:'block' }}
-      title="Northeast Climate Dashboard"
-    />
-  )
-}
